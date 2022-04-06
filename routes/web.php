d2658c2867241f2d0c36904923ccba9fb98aacf1
TEI <reinis.tei@gmail.com>
2022-04-06 19:33:33 +0300

    LLR-5  Laravel Section 5 06/04/2022

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('posts', ['posts'=>Post::latest()->with(['category', 'author'])->get()
+    return view('posts', [
+        'posts'=>Post::latest()->with(['category', 'author'])->get(),
+        'categories' => Category::all()
     ]);
-});
+})->name('home');
 
 Route::get('posts/{post:slug}', function (Post $post) { //Post::where('slug', $post->firstorfail();
     return view('post', [
@@ -40,12 +42,15 @@ Route::get('posts/{post:slug}', function (Post $post) { //Post::where('slug', $p
 
 Route::get('categories/{category:slug}', function(Category $category) { //route model binding - wildcard name must match variable
     return view('posts', [
-        'posts'=>$category->posts->load(['category', 'author'])
+        'posts'=>$category->posts->load(['category', 'author']),
+        'categories' => Category::all(),
+        'currentCategory' => $category
     ]);
-});
+})->name('category');
 
 Route::get('/authors/{author:username}', function(User $author) { //route model binding - wildcard name must match variable
     return view('posts', [
-        'posts'=>$author->posts->load(['category', 'author'])
+        'posts'=>$author->posts->load(['category', 'author']),
+        'categories' => Category::all()
     ]);
 });
